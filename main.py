4bbfaa54287a1991ab33a5200861d3720df8cfd6
Trivedi Rutvik <46040223+rutvik29@users.noreply.github.com>
2024-08-02 11:45:46 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
 
+from flask import Flask, request, render_template
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        if 'file' in request.files:
+            file = request.files['file']
+            data = pd.read_csv(file)
+        else:
+           
+            pass
+
+        model = LinearRegression()
+        X = data[['feature1', 'feature2']]  
+        y = data['target']  
+        model.fit(X, y)
+        predictions = model.predict(X)
+        
+        return render_template('results.html', predictions=predictions)
+
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
